5e851387ebbc73d4df0972348aadc75c0e2d1667
yangsisi002 <yangsisi002@lianjia.com>
2018-06-04 00:33:20 +0800

    add

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+//console.log("111111",path.resolve('dist'))
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve('dist'),
+      filename: '[name].js',
+      chunkFilename: 'js/[name].js',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+           { 
+               test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ],
+        
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            title: 'webpack ejs demo',
+            inject: false,
+            appMountId: 'root',
+            template: './src/template/index.ejs',
+        })
+    ]
+  };
